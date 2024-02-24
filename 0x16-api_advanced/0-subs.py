7519a9f83cb9b3bdea450a29c384a4f132b96812
Holladworld <oladimejiolayinka@gmail.com>
2024-02-24 05:48:59 -0800

    Reddit subReddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+Queries the Reddit API and returns
+the number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    headers = {'User-agent': 'Hollad'}
+    URL = 'https://www.reddit.com/r/{}/about.json'
+    get_sudreddit = requests.get(URL.format(subreddit),
+                                 headers=headers, allow_redirects=False)
+    if get_sudreddit.status_code != 200:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    return get_sudreddit.json().get('data', {}).get('subscribers')
